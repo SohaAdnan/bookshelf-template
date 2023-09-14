e5eb3137317e9c6d07af3f734c837db55b461f3b
SohaAdnan <soha.albardawil@email.com>
2023-09-14 15:26:42 +0300

    Include connect to db

--- main.go
+++ main.go
@@ -3,20 +3,28 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/ilhamsyahids/bookshelf-template/rest"
 	"github.com/ilhamsyahids/bookshelf-template/storage"
+	"github.com/jmoiron/sqlx"
 )
 
 const addr = ":8080"
 
+const envSQLDSN = "SQL_DSN"
+
 func main() {
-	// init storage
-	storage := storage.NewStorage()
-	err := storage.Load("books.json")
+	sqlDSN := os.Getenv(envSQLDSN)
+	sqlClient, err := sqlx.Connect("mysql", sqlDSN)
 	if err != nil {
-		log.Fatalf("unable to load storage due: %v", err)
+		log.Fatalf("unable to connect to database due: %v", err)
 	}
+	defer sqlClient.Close()
+
+	// init storage
+	storage := storage.NewStorage(sqlClient)
 
 	// init API
 	api, err := rest.NewAPI(rest.APIConfig{BookStorage: *storage})
